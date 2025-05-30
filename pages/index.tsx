995c1e2927d521151e54201ee527a02ecb8ef359
danieltapasco <daniel.tapasco@ucp.edu.co>
2025-05-30 18:22:01 -0500

    Primer commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Formulario from '@/components/FormGeneral';
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-blue-900 via-blue-700 to-blue-500 flex items-center justify-center py-20 px-4">
-      <Formulario />
+      <Formulario/>
     </main>
   );
 }
