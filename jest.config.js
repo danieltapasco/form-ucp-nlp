8751f84426fab40d0dfd28c4565ed6c325c3a744
danieltapasco <daniel.tapasco@ucp.edu.co>
2025-05-30 19:14:40 -0500

    Primer commit

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-  reporters: [
-    "default",
-    ["jest-junit", { outputDirectory: "./", outputName: "junit.xml" }],
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
   ],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 };
+
+module.exports = createJestConfig(customJestConfig);
